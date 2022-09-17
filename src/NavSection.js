5a2528daf3ba17b36a895db88ba63d76066ad1e4
Gao-N <nilsongao33@gmail.com>
2022-09-17 00:41:32 -0400

    initial-commit

---
+++ src/NavSection.js
@@ -0,0 +1,3 @@
+export default function navSection({title}){
+    return <section className="text-white font-bold tracking-[0.1875em]">{title}</section>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'emerald': '#4e937a',
+      'opal': '#9abdb2',
+      'onyx': '#40403f',
+      'prussian': '#12263a',
+      'gainsboro': '#e1e1e4',
+      'blue': '#8499b9',
+      'black': '#000000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
