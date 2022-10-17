967879e93c4030cd9be1b65e19c168352f7738b0
Gao-N <nilsongao33@gmail.com>
2022-10-17 18:15:33 -0400

    finished responsive design, added modularity of links, and removed a nav section header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 //import logo from './logo.svg';
 import Button from './Button';
+import LinkButton from './LinkButton';
 import NavItem from './NavItem';
 export default function App() {
   return (
     <div className='scroll-smooth'>
-      <nav className='fixed flex justify-around items-center bg-onyx min-h-13 p-3 w-screen'>
-        <div className='flex justify-around tracking-[.25em] w-screen'>
+      <nav className='fixed flex justify-around items-center bg-onyx p-3 w-full'>
+        <div className='flex justify-around tracking-[.10em] w-screen'>
           <a href='/'><h1 className='text-lg text-white font-bold hover:text-black tracking-widest'>Nilson Gao</h1></a>
           <ul className='flex justify-around w-7/12 scroll-smooth'>
-            <NavItem title="About Me" href={"#Summary"}></NavItem>
+            {/* <NavItem title="About Me" href={"#Summary"}></NavItem> */}
             <NavItem title="Skills" href={"#Skills"}></NavItem>
             <NavItem title="Courses" href={"#Courses"}></NavItem>
             <Button title="Contact" href={"mailto:nilsongao33@gmail.com"}></Button>
@@ -21,15 +22,15 @@ export default function App() {
           <img src='1653540266234.jfif' alt='profile' width={300}></img>
         </div>
         <div className='flex flex-col bg-gainsboro h-4/5 w-2/3'>
-          <p className='text-lg font-bold text-left m-10'>I am a second year student at the University of Toronto Scarborough.
+          <p className='text-sm font-bold text-left m-10 sm:text-lg'>I am a second year student at the University of Toronto Scarborough.
               I am currently in Statistics Specialist - Machine Learning and Data Mining program. I wish to pursue a career that allows me to use and manipulate Data to achieve desired results.
               I believe we live in a time where there is abundant information and inefficient usage of it. I am observant, willing to learn, passionate, straightforward, and organized.
               I strive under pressure and enjoy overcoming challenges.
           </p>
           <div className='flex justify-center'>
-            <a href='https://github.com/Gao-N' className="text-lg text-center text-white bg-emerald font-bold tracking-normal rounded-full hover:bg-opal hover:text-black w-1/6 p-2 m-2">Github</a>
-            <a href='https://www.linkedin.com/in/nilson-gao/' className="text-base text-center text-white bg-emerald font-bold tracking-normal rounded-full hover:bg-opal hover:text-black w-1/6 p-2 m-2">Linkedin</a>
-            <a href='/Nilson_Gao_Resume_28-09-2022-16-07-39.pdf' target="_blank" className="text-lg text-center text-white bg-emerald font-bold tracking-normal rounded-full hover:bg-opal hover:text-black w-1/6 p-2 m-2">Resume</a>
+            <LinkButton href={'https://github.com/Gao-N'} title="Github"></LinkButton>
+            <LinkButton href={'https://www.linkedin.com/in/nilson-gao/'} title="Linkedin"></LinkButton>
+            <LinkButton href={'/Nilson_Gao_Resume_28-09-2022-16-07-39.pdf'} title="Resume"></LinkButton>
           </div>
         </div>
       </section>
--- src/NavItem.js
+++ src/NavItem.js
@@ -1,3 +1,3 @@
 export default function navSection({title, href}){
-    return <li className="list-none pt-3"><a href={href} className="text-white align-text-bottom bg-emerald font-bold content-bottom tracking-normal rounded-lg p-6 hover:bg-opal hover:text-black" >{title}</a></li>
+    return <li className="list-none pt-3"><a href={href} className="text-white align-text-bottom bg-emerald font-bold content-bottom tracking-normal rounded-lg p-5 hover:bg-opal hover:text-black" >{title}</a></li>
 }
