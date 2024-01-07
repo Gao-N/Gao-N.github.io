17939b7679953fac7fcd1310d67dde493e572057
Gao-N <nilsongao33@gmail.com>
2024-01-06 21:18:51 -0500

    fixed bugs regarding sections

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ import LinkButton from './LinkButton';
 import NavItem from './NavItem';
 export default function App() {
   return (
-    <div className='scroll-smooth'>
+    <div className='scroll-smooth md:scroll-smooth'>
       <nav className='fixed flex justify-around items-center bg-onyx p-2 w-full'>
         <div className='flex justify-around tracking-tighter sm:tracking-[.10em] w-full'>
           <a href='/'><h1 className='text-base sm:text-lg m-2 text-white font-bold hover:text-black tracking-tight'>Nilson Gao</h1></a>
           <ul className='flex justify-around w-7/12 scroll-smooth'>
-            {/* <NavItem title="About Me" href={"#Summary"}></NavItem> */}
-            <NavItem title="Summary" href={"#Skills"}></NavItem>
-            <NavItem title="Skills" href={"#Courses"}></NavItem>
-            <NavItem title="Courses" href={"#Interviews"}></NavItem>
+            <NavItem title="Summary" href={"#Summary"}></NavItem>
+            <NavItem title="Skills" href={"#Skills"}></NavItem>
+            <NavItem title="Courses" href={"#Courses"}></NavItem>
             <Button title="Contact" href={"mailto:nilsongao33@gmail.com"}></Button>
           </ul>
         </div>
       </nav>
       <section id = "Summary" className='bg-opal flex justify-evenly items-center content-center h-screen w-full'>
         <div className='flex flex-col justify-center'>
-          <h1 className='text-2xl font-bold text-center mb-4'>About Me</h1>
+          <h1 className='text-3xl font-bold text-center mb-4'>About Me</h1>
           <img src='1653540266234.jfif' alt='profile' width={300}></img>
         </div>
-        <div className='flex flex-col bg-gainsboro h-4/5 w-2/3'>
+        <div className='flex flex-col bg-gainsboro h-3/5 w-2/3'>
           <p className='text-sm font-bold text-left m-10 sm:text-lg'>I am a student at the University of Toronto Scarborough currently studying in Statistics - ML and DataSci program. I wish to pursue a career that allows me to use and manipulate Data to achieve desired results.
               I believe we live in a time where data skills are becoming increasingly important. I am observant, willing to learn, passionate, and organized.
               I adapt quickly under pressure and enjoy overcoming challenges.
@@ -35,7 +34,7 @@ export default function App() {
         </div>
       </section>
       <section id = "Skills" className='bg-blue flex justify-evenly items-center content-center h-screen w-full'>
-        <h1 className='text-2xl font-bold text-black text-center'>Summary of Skills</h1>
+        <h1 className='text-4xl font-bold text-black text-center'>Summary of Skills</h1>
         <div className='flex justify-center bg-onyx h-4/5 w-4/6 text-center text-white place-content-evenly'>
         <ol className='text-sm sm:text-lg font-bold text-center m-10 list-none'>
           <li className='h-min sm:mb-6 mb-11'>Python</li>
@@ -50,7 +49,7 @@ export default function App() {
         </div>
       </section>
       <section id = "Courses" className='bg-prussian flex justify-evenly items-center content-center h-screen w-full'>
-        <h1 className='text-2xl font-bold text-white text-center'>Relevant Courses</h1>
+        <h1 className='text-4xl font-bold text-white text-center'>Relevant Courses</h1>
         <div className='flex justify-evenly items-center bg-gainsboro h-4/5 w-4/6'>
         <ul>
           <li className='bg-emerald rounded-md m-2 p-2'><h1 className='font-bold text-2xl text-white'>CSCA48</h1>
